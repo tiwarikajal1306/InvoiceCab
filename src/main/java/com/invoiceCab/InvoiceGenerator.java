2d0791128ddf55ee934b0356d8eeb0577d8bfac4
kajal <tiwarikajal1306@gmail.com>
2020-07-11 01:45:21 +0530

    ADDED Calculating minimum fair

--- src/main/java/com/invoiceCab/InvoiceGenerator.java
+++ src/main/java/com/invoiceCab/InvoiceGenerator.java
@@ -4,7 +4,12 @@ public class InvoiceGenerator {
 
         private static final double MIN_COST_PER_KM = 10;
         private static final int COST_PER_TIME = 1;
+        private static final double MIN_FARE = 5;
+
         public double calculateFare(double distance, int time) {
-            return distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+            double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+            if (totalFare < MIN_FARE)
+                return MIN_FARE;
+            return totalFare;
         }
 }
--- src/test/java/com/invoiceCab/InvoiceGeneratorTest.java
+++ src/test/java/com/invoiceCab/InvoiceGeneratorTest.java
@@ -12,4 +12,12 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare,0.0);
     }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare,0.0);
+    }
 }
