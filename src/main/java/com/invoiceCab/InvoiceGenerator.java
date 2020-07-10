c42ff3e07522b91dd6d4713911696eccbbba69f2
kajal <tiwarikajal1306@gmail.com>
2020-07-11 01:37:27 +0530

    ADDED Calculated total fair

--- src/main/java/com/invoiceCab/InvoiceGenerator.java
+++ src/main/java/com/invoiceCab/InvoiceGenerator.java
@@ -1,4 +1,10 @@
 package com.invoiceCab;
 
 public class InvoiceGenerator {
+
+        private static final double MIN_COST_PER_KM = 10;
+        private static final int COST_PER_TIME = 1;
+        public double calculateFare(double distance, int time) {
+            return distance * MIN_COST_PER_KM + time * COST_PER_TIME;
+        }
 }
--- src/test/java/com/invoiceCab/InvoiceGeneratorTest.java
+++ src/test/java/com/invoiceCab/InvoiceGeneratorTest.java
@@ -1,5 +1,15 @@
 package com.invoiceCab;
 
-public class InvoiceGeneratorTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class InvoiceGeneratorTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare,0.0);
+    }
 }
